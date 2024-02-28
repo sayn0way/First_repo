462022a164e14b891f91ccd25c48f71221f3f7b9
sayn0way <stanverbickij@gmail.com>
2024-02-28 18:09:24 +0200

    3rd commit

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 print ("Hello world")
 
 print ("Hello Git")
+
+name = "Олег"
+hello_string = f"Hello,{name}"
+print (hello_string)
+
+age = 18
+adult1 = age >= 18
+adult2 = age < 18
+
+print (adult1)
+print (adult2)
+
+a = input("Рядок запрошення:")
+
+print (a)
