9f36943fd6a31a5fd68564bf685ad2fd9cfa704f
sayn0way <stanverbickij@gmail.com>
2024-02-28 18:31:00 +0200

    2nd commit

--- hello.py
+++ hello.py
@@ -1,2 +1,15 @@
-print ("Hello World!!!")
-input()
+age = input ("How old are you? ")
+
+if int(age) >= 18:
+    print ("You are adult already.")
+else:
+    print ("You are infant yet.")
+
+a = input('Введіть число: ')
+a = int(a)
+if a > 0:
+    print("Число додатне")
+elif a < 0:
+    print("Число від'ємне")
+else:
+    print("Це число - нуль (зееееро)")
